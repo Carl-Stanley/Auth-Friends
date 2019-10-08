c307844227d7531d11b210d82685a197e4bf8256
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-10-08 07:39:54 -0700

    commit

---
+++ friends/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function TheHeader() {
+
+    return (
+
+        <div className="appHeader"><h1>*** Auth friends ***</h1></div>
+        
+    )
+
+}
+
+export default TheHeader;
